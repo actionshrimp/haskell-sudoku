79d1ba07898b98f6c4b4c4237a91db9c268bbddc
Dave Aitken <dave.aitken@gmail.com>
2015-05-05 07:57:21 +0100

    WIP

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,56 @@
 module Main where
 
 import Control.Monad.Except
+import UI.NCurses
 
 data Sn = Blank | Sn Int
 type SRow = [Sn]
 type SGrid = [SRow]
 
+instance Show Sn where
+    show Blank = "_"
+    show (Sn n) = show n
+
 data CreateGridError = IllegalGridSize
 instance Show CreateGridError where
     show IllegalGridSize = "Illegal grid size!"
 
-type CreateGridM = Either CreateGridError
-
 isSquare :: Int -> Bool
 isSquare n = rt * rt == n
-    where rt = truncate (sqrt x)
-          x = fromIntegral n
+    where rt = truncate . sqrt . fromIntegral $ n
 
-blankGrid :: Int -> CreateGridM SGrid
+blankGrid :: Int -> Either CreateGridError SGrid
 blankGrid n | isSquare n = return (take n (repeat (take n (repeat Blank))))
             | otherwise = throwError IllegalGridSize
 
-main = print "lol"
+drawGrid :: SGrid -> Update ()
+drawGrid [] = return ()
+drawGrid (r:rs) = do
+    drawRow (length r) r
+    drawGrid rs
+
+drawRow l (n:ns) = do
+    drawString "_"
+
+drawError :: CreateGridError -> Update ()
+drawError e = do
+    drawString $ show e
+
+main :: IO ()
+main = runCurses $ do
+    setEcho False
+    w <- defaultWindow
+    updateWindow w $ do
+        moveCursor 1 10
+        blankGrid 9 `catchError` drawError
+    render
+    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+
+waitFor :: Window -> (Event -> Bool) -> Curses ()
+waitFor w p = loop where
+    loop = do
+        ev <- getEvent w Nothing
+        case ev of
+            Nothing -> loop
+            Just ev' -> if p ev' then return () else loop
+
