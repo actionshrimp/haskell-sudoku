42575540515077b41861ab0dc8fa15266aa5ed24
Dave Aitken <dave.aitken@gmail.com>
2015-05-14 00:01:00 +0100

    A big mess

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Control.Monad.Except
-import Control.Monad.State
+import Data.Maybe
 import UI.NCurses
+import System.Environment
+import Data.Text (Text, unpack)
+import Text.Read (readMaybe)
 
 data Sn = Blank | Sn Int
 type SRow = [Sn]
@@ -12,9 +16,6 @@ instance Show Sn where
     show Blank = "_"
     show (Sn n) = show n
 
-data CreateGridError = IllegalGridSize
-instance Show CreateGridError where
-    show IllegalGridSize = "Illegal grid size!"
 
 isSquare :: Int -> Bool
 isSquare n = rt * rt == n
@@ -22,55 +23,93 @@ isSquare n = rt * rt == n
 
 blankGrid :: Int -> Either CreateGridError SGrid
 blankGrid n | isSquare n = return (take n (repeat (take n (repeat Blank))))
-            | otherwise = throwError IllegalGridSize
+            | otherwise = throwError GridNotSquare
 
-data CursorState = CursorState {
-    row :: Integer,
-    col :: Integer
-}
+cellH :: Integer
+cellH = 3
+
+cellW :: Integer
+cellW = 5
 
 drawGrid :: SGrid -> Update ()
-drawGrid g = evalStateT (drawGridS g) CursorState {
-    row=0, col=0
-}
+drawGrid g = do
+    drawRows 0 g
 
-drawGridS :: SGrid -> StateT CursorState Update ()
-drawGridS [] = return ()
-drawGridS (r:rs) = do
-    cur <- get
-    lift $ moveCursor (row cur) 0
-    drawRowS r
-    modify (\s -> s { row = (row s + 1) })
-    drawGridS rs
+drawRows :: Integer -> SGrid -> Update ()
+drawRows _ [] = return ()
+drawRows i (r:rs) = do
+    drawCells i 0 r
+    drawRows (i + 1) rs
 
-drawRowS :: SRow -> StateT CursorState Update ()
-drawRowS [] = return ()
-drawRowS (n:ns) = do
-    cur <- get
-    lift $ drawString "_ "
-    modify (\s -> s { col = (col s + 1) })
-    drawRowS ns
+drawCells :: Integer -> Integer -> SRow -> Update ()
+drawCells _ _ [] = return ()
+drawCells i j (c:cs) = do
+    drawCell i j c
+    drawCells i (j + 1) cs
+
+drawCell :: Integer -> Integer -> Sn -> Update ()
+drawCell i j c = do
+    moveCursor (i * cellH) (j * cellW)
+    drawLineH Nothing cellW
+    drawLineV Nothing cellH
+    moveCursor ((i + 1) * cellH) (j * cellW)
+    drawLineH Nothing cellW
+    moveCursor (i * cellH) ((j + 1) * cellW)
+    drawLineV Nothing cellH
 
 drawError :: CreateGridError -> Update ()
 drawError e = do
     drawString $ show e
 
-main :: IO ()
-main = runCurses $ do
-    setEcho False
-    w <- defaultWindow
-    updateWindow w $ do
-        case (blankGrid 9) of
-            Right a -> drawGrid a
-            Left e -> drawError e
-    render
-    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+class SudokuError a where
+    errMsg :: a -> Text
 
-waitFor :: Window -> (Event -> Bool) -> Curses ()
-waitFor w p = loop where
-    loop = do
-        ev <- getEvent w Nothing
-        case ev of
-            Nothing -> loop
-            Just ev' -> if p ev' then return () else loop
+data CreateGridError = GridNotSquare
+showGridError :: CreateGridError -> Text
+showGridError GridNotSquare = "Sudoku grid must be a square number"
+
+instance SudokuError CreateGridError where
+    errMsg = showGridError
+
+instance Show CreateGridError where
+    show = unpack . showGridError
+
+liftEither :: Monad m => Either e a -> ExceptT e m a
+liftEither x = ExceptT (return x)
+
+sudoku :: (SudokuError e) => [String] -> ExceptT e IO ()
+sudoku [] = liftIO $ putStrLn "Please pass a number"
+sudoku (a:as) = do
+    case (readMaybe a :: Maybe Int) of
+        Just n -> liftIO $ putStrLn a
+        Nothing -> liftIO $ putStrLn ("Not a number: " ++ a)
+    return ()
+
+main :: IO ()
+main = do
+    args <- getArgs
+    runExceptT $ ((sudoku args) :: ExceptT CreateGridError IO ())
+    return ()
+
+--main :: IO ()
+--main = do
+--    a:args <- getArgs
+--    g <- blankGrid a
+--    runCurses $ do
+--        setEcho False
+--        w <- defaultWindow
+--        updateWindow w $ do
+--            case (blankGrid 9) of
+--                Right g -> drawGrid g
+--                Left e -> drawError e
+--        render
+--        waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+--
+--waitFor :: Window -> (Event -> Bool) -> Curses ()
+--waitFor w p = loop where
+--    loop = do
+--        ev <- getEvent w Nothing
+--        case ev of
+--            Nothing -> loop
+--            Just ev' -> if p ev' then return () else loop
 
