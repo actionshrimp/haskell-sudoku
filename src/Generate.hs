1000a1d8bf5fd93fed4006be533e4cc1c605c2d1
Dave Aitken <dave.aitken@gmail.com>
2015-06-09 19:22:19 +0100

    Use MonadRandom instead of State

--- src/Generate.hs
+++ src/Generate.hs
@@ -1,24 +1,31 @@
 module Generate where
 import System.Random
-import GameState
-import Rules
+import Control.Monad.Random
+--import GameState
+--import Rules
 
 gen :: StdGen
 gen = mkStdGen 11
 
-randomRow :: StdGen -> ([SEntry], StdGen)
+randomRow :: RandomGen g => Rand g [Int]
 randomRow = aux [] where
-    aux as g | length as == 9 = (map (Right . toEnum) as, g)
-             | a `elem` as = aux as g'
-             | otherwise = aux (a:as) g'
-      where (a, g') = randomR (1, 9) g
+    aux as = if length as == 9 then return as else do
+          a <- getRandomR (1, 9)
+          aux (a:as)
 
-randomGrid :: StdGen -> ([[SEntry]], StdGen)
-randomGrid = aux [] where
-    aux rs g | length rs == 9 = (rs, g)
-             | otherwise = aux (r:rs) g'
-      where (r, g') = randomRow g
+--addRandomRow :: [[SEntry]] -> State StdGen [[SEntry]]
+--addRandomRow [] = do 
+--    g <- get
+--    let (r, g') = runState randomRow g in do
+--      put g'
+--      return r:[]
 
-sudokuGrid :: StdGen -> ([[SEntry]], StdGen)
-sudokuGrid g = let (grid, g') = randomGrid g in
-    if valid grid then (grid, g') else sudokuGrid g'
+--randomGrid :: State StdGen [SEntry]
+--randomGrid = aux [] where
+--    aux rs g | length rs == 9 = (rs, g)
+--             | otherwise = aux (r:rs) g'
+--      where (r, g') = randomRow g
+--
+--sudokuGrid :: StdGen -> ([[SEntry]], StdGen)
+--sudokuGrid g = let (grid, g') = randomGrid g in
+--    if valid grid then (grid, g') else sudokuGrid g'
