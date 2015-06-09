372ce6ab02425c47a3e77b17da361443dc0d61c5
Dave Aitken <dave.aitken@gmail.com>
2015-06-09 18:40:21 +0100

    Some randomness

--- src/GameState.hs
+++ src/GameState.hs
@@ -2,9 +2,11 @@ module GameState where
 
 import Data.Default (Default, def)
 import Control.Lens
+import Data.Tuple (swap)
+import Data.Maybe (fromJust)
 
 data S_ = S_ deriving (Eq, Ord)
-data Sn = S1 | S2 | S3 | S4 | S5 | S6 | S7 | S8 | S9 deriving (Enum, Eq, Ord)
+data Sn = S1 | S2 | S3 | S4 | S5 | S6 | S7 | S8 | S9 deriving (Eq, Ord)
 type SEntry = Either S_ Sn
 data GS = GS {
     cursor :: (Int, Int),
@@ -24,6 +26,13 @@ instance Show Sn where
     show S7 = "7"
     show S8 = "8"
     show S9 = "9"
+
+instance Enum Sn where
+    fromEnum = fromJust . flip lookup snTable
+    toEnum = fromJust . flip lookup (map swap snTable)
+
+snTable :: [(Sn, Int)]
+snTable = [(S1, 1), (S2, 2), (S3, 3), (S4, 4), (S5, 5), (S6, 6), (S7, 7), (S8, 8), (S9, 9)]
 
 instance Default GS where
     def = GS (0, 0) (replicate 9 (replicate 9 (Left S_)))
--- src/Generate.hs
+++ src/Generate.hs
@@ -1,15 +1,24 @@
 module Generate where
 import System.Random
 import GameState
+import Rules
 
 gen :: StdGen
 gen = mkStdGen 11
 
-mkRandomRow :: StdGen -> [Int] -> ([Int], StdGen)
-mkRandomRow g as | length as == 9 = (as, g)
-                 | a `elem` as = mkRandomRow g' as
-                 | otherwise = mkRandomRow g' (a:as)
-  where (a, g') = randomR (1, 10) g
+randomRow :: StdGen -> ([SEntry], StdGen)
+randomRow = aux [] where
+    aux as g | length as == 9 = (map (Right . toEnum) as, g)
+             | a `elem` as = aux as g'
+             | otherwise = aux (a:as) g'
+      where (a, g') = randomR (1, 9) g
 
---randomGrid :: StdGen -> ([[Int]], StdGen)
---randomGrid 
+randomGrid :: StdGen -> ([[SEntry]], StdGen)
+randomGrid = aux [] where
+    aux rs g | length rs == 9 = (rs, g)
+             | otherwise = aux (r:rs) g'
+      where (r, g') = randomRow g
+
+sudokuGrid :: StdGen -> ([[SEntry]], StdGen)
+sudokuGrid g = let (grid, g') = randomGrid g in
+    if valid grid then (grid, g') else sudokuGrid g'
