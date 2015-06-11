f2682b51b44d8d9ae93c18fc13f3b2e2b8623721
Dave Aitken <dave.aitken@gmail.com>
2015-06-12 00:19:52 +0100

    WIP on randomGrid

--- src/Generate.hs
+++ src/Generate.hs
@@ -1,31 +1,39 @@
 module Generate where
 import System.Random
 import Control.Monad.Random
+import Data.List.Split (chunksOf)
+import Control.Monad (foldM)
 --import GameState
 --import Rules
 
 gen :: StdGen
 gen = mkStdGen 11
 
-randomRow :: RandomGen g => Rand g [Int]
-randomRow = aux [] where
-    aux as = if length as == 9 then return as else do
-          a <- getRandomR (1, 9)
-          aux (a:as)
+block :: [[Int]] -> (Int, Int) -> [Int]
+block grid (r, c) = let
+    bR = r `div` 3
+    bC = c `div` 3
+    bRow = take 3 . drop (3 * bR) $ grid
+    in
+      concatMap (take 3 . drop (3 * bC)) bRow
 
---addRandomRow :: [[SEntry]] -> State StdGen [[SEntry]]
---addRandomRow [] = do 
---    g <- get
---    let (r, g') = runState randomRow g in do
---      put g'
---      return r:[]
+allowedFor :: [Int] -> (Int, Int) -> [Int]
+allowedFor g (r, c) = filter notUsed [1..9] where
+    grid = chunksOf 9 g
+    row | r >= (length grid - 1) = []
+        | otherwise = grid !! r
+    col = map (!! c) grid 
+    bl = block grid (r, c)
+    notUsed x = x `notElem` row && x `notElem` col && x `notElem` bl
 
---randomGrid :: State StdGen [SEntry]
---randomGrid = aux [] where
---    aux rs g | length rs == 9 = (rs, g)
---             | otherwise = aux (r:rs) g'
---      where (r, g') = randomRow g
---
+randomAllowed :: RandomGen g => [Int] -> (Int, Int) -> Rand g Int
+randomAllowed g c = fromList $ map (\x -> (x, 1)) $ allowedFor g c
+
+randomGrid :: Rand g [Int]
+randomGrid = let
+    coords = concat [[(r, c) | c <- [0..8]] | r <- [0..8]]
+    in foldM (\acc x -> randomAllowed acc x) : acc) [] coords
+
 --sudokuGrid :: StdGen -> ([[SEntry]], StdGen)
 --sudokuGrid g = let (grid, g') = randomGrid g in
 --    if valid grid then (grid, g') else sudokuGrid g'
